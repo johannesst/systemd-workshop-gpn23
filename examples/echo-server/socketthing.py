687e7cf2bfb5d77d300189a54bc0f12284db71db
Johannes Starosta <johannes.starosta@gmail.com>
2025-06-18 14:29:08 +0200

    Added examples

---
+++ examples/echo-server/socketthing.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# "Borrowed" from Paul Brown
+# https://www.linux.com/training-tutorials/end-road-systemds-socket-units/
+import sys
+sys.stdout.write(sys.stdin.readline().strip().upper() + 'rn')
