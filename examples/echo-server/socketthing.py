b8c4c42277439dd7cb77564274badaaf2d23894a
Johannes Starosta <johannes.starosta@gmail.com>
2025-06-22 00:26:11 +0200

    Updated examples

--- examples/echo-server/socketthing.py
+++ examples/echo-server/socketthing.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+# Beispiel für socket-activation mit systemd 
+# GPN Workshop 22. Juni 2025
 # "Borrowed" from Paul Brown
 # https://www.linux.com/training-tutorials/end-road-systemds-socket-units/
 import sys
